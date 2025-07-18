9a59a534726e6fef2c6d347ffcb1fde4838e8281
BeingDIABLO <sarthakverma110@gmail.com>
2025-07-19 02:31:41 +0530

    d4_Rock,paper,scissor.py

--- d4_Rock,paper,scissor.py
+++ d4_Rock,paper,scissor.py
@@ -1,85 +1,46 @@
 import random
-n=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
-if n==0:
-    print(''' You Choosed rock
+
+rock = '''Rock
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-''')
-elif n==1:
-    print('''You Choosed Paper
+'''
+
+paper = '''Paper
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-''')
+'''
 
-elif n==2:
-    print('''You Choosed Scissor
+scissors = '''Scissors
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-''')
-else:
+'''
+game_icons=[rock, paper, scissors]
+user_input=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: "))
+if(user_input>=3 or user_input<=-1):
     print("Invalid choice, So Computer Won by default")
-random_choice=random.randint(0,2)
+else:
+    print(f"You choose",game_icons[user_input])
 
-if(random_choice==0):
-    print(''' Computer Choosed rock
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-''')
-elif random_choice==1:
-    print('''Computer Choosed Paper
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-''')
+    
+computer_choice=random.randint(0,2)
+print(f"Conpter Choose",game_icons[computer_choice])
 
-elif(random_choice==2):
-    print('''Computer Choosed Scissor
-    _______
----'   ____)____
-          ______)
-       __________)
-      (____)
----.__(___)
-''')
-
-if(n==0 and random_choice==0 or n==1 and random_choice==1 or n==2 and random_choice==2):
+if(user_input==0 and computer_choice==0 or user_input==1 and computer_choice==1 or user_input==2 and computer_choice==2):
     print("It's a draw")
-elif(n==0 and random_choice==2 or n==1 and random_choice==0 or n==2 and random_choice==1):
+elif(user_input==0 and computer_choice==2 or user_input==1 and computer_choice==0 or user_input==2 and computer_choice==0):
     print("You Won")
 else:
     print("Computer Won")
     
-
-
-    
-
-    
-
-
-
-
-
-
-
-
-
-
